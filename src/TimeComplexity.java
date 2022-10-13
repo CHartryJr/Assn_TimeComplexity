786d5fb2ac123f6cf16bcb1a775633bb46c4ac0b
carl hartry jr <carlhartry+github@gmail.com>
2022-10-13 18:58:29 -0400

    Main changes

--- src/TimeComplexity.java
+++ src/TimeComplexity.java
@@ -2,28 +2,65 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.nio.file.FileSystems;
+import java.util.Random;
 
 public class TimeComplexity {
-    private static final  int MULTIPLIER = 10;
+    private static final int MULTIPLIER = 10;
+    private static final int MAX_INTERVALS = 4;
     private static int inputs = 1000000;
-    private static int[] arr;
-
+    
     public static void main(String[] args) {
-        arr = new int[inputs] ;
-        String location  = FileSystems.getDefault().getPath("resultsForTime").toAbsolutePath().toString()+"/Results.txt";
-        String Message ="";
+        String location = FileSystems.getDefault().getPath("resultsForTime").toAbsolutePath().toString();
+        String Message = "";
+        long startTime;
+        long endTime;
+        for (int i = 0; i < MAX_INTERVALS; i++) {
+            int[] sorted = createlist(true);
+            int[] unsorted = createlist(false);
+            for (SortingAlgos.sortNames x : SortingAlgos.sortNames.values()) {
+                switch (x) {
+                    case SELECTION:
+                        break;
+                    case INSERTION:
+                        break;
+                    case BUBBLE:
+                        break;
+                    case MERGESORT:
+                        break;
+                    case QUICKSORT:
+                        break;
+                }
+            }
+            inputs *= MULTIPLIER;
+        }
     }
 
     protected static boolean writedToFile(String fileLocation, String message) {
+        PrintWriter editor = null;
         try {
-            PrintWriter editor = new PrintWriter(new BufferedWriter(new FileWriter(fileLocation, true)));
+            editor = new PrintWriter(new BufferedWriter(new FileWriter(fileLocation, true)));
             editor.write(message);
             editor.close();
-            System.out.println("Data has been written to " + fileLocation);
             return true;
         } catch (Exception e) {
+            editor.close();
             e.printStackTrace();
         }
         return false;
+    }
+
+    private static int[] createlist(boolean sorted) {
+        int[] arr = new int[inputs];
+        if (sorted) {
+            for (int i = 0; i < inputs; i++) {
+                arr[i] = i;
+            }
+        } else {
+            Random rand = new Random();
+            for (int i = 0; i < inputs; i++) {
+                arr[i] = rand.nextInt();
+            }
+        }
+        return arr;
     }
 }
